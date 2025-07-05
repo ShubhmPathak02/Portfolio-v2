1fa69d635bd6e7167ecac60ea2b7ba347dc3e332
unknown <24bec100@nith.ac.in>
2025-07-06 03:35:51 +0530

    Fix: added base path for Vite deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Detect production (GitHub Pages) or development (localhost)
-const base = process.env.NODE_ENV === "production" ? "/Portfolio-v2/" : "/";
-
 export default defineConfig({
   plugins: [react()],
-  base: base,
+  base: "/",
 });
